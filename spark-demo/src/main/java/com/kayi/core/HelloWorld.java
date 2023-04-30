ff98d68c2801a28cd9d2b8fffd00f3fdc7a3fdea
nql <386823469@qq.com>
2023-05-01 01:47:14 +0800

    测试

---
+++ spark-demo/src/main/java/com/kayi/core/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.kayi.core;
+
+/**
+ * ClassName: HelloWorld
+ * package: com.kayi.core
+ * Description:
+ *
+ * @Author: nql
+ * @Create:2023/5/1 - 1:45
+ */
+public class HelloWorld {
+
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
